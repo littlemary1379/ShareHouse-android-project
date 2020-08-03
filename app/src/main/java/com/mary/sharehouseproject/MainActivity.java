30f55c08fde0f212ea68fe5f38f0e967ba1a9e02
Mary <qldhffk123@gmail.com>
2020-08-04 01:31:44 +0900

    헤더 액션 적용

--- app/src/main/java/com/mary/sharehouseproject/MainActivity.java
+++ app/src/main/java/com/mary/sharehouseproject/MainActivity.java
@@ -1,6 +1,8 @@
 package com.mary.sharehouseproject;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -8,7 +10,10 @@ import android.content.pm.Signature;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import net.daum.android.map.MapView;
 
@@ -16,12 +21,35 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "Main_Activity";
+
+    private ImageView ivHamburgerButton;
+    private DrawerLayout mainDrawerLayout;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        init();
+        setSupportActionBar(toolbar);
+        initListener();
+    }
 
+    private void init(){
+        ivHamburgerButton=findViewById(R.id.iv_hamburgerButton);
+        mainDrawerLayout=findViewById(R.id.layout_mainDrawer);
+        toolbar=findViewById(R.id.toolbar_main);
+    }
+
+    private void initListener(){
+        ivHamburgerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: 햄버거 버튼 클릭됨");
+                mainDrawerLayout.openDrawer(Gravity.LEFT);
+            }
+        });
     }
 }
