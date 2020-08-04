d64a1274d52088c86db6120f02a8aeee13e45e1a
mary <qldhffk112@gmail.com>
2020-08-04 16:33:48 +0900

    입주절차 프레그먼트 제작

--- app/src/main/java/com/mary/sharehouseproject/MainActivity.java
+++ app/src/main/java/com/mary/sharehouseproject/MainActivity.java
@@ -1,10 +1,12 @@
 package com.mary.sharehouseproject;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -13,9 +15,12 @@ import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
 import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+
+import com.google.android.material.navigation.NavigationView;
 
 import net.daum.android.map.MapView;
 
@@ -27,7 +32,9 @@ public class MainActivity extends AppCompatActivity {
 
     private ImageView ivHamburgerButton, ivToolbarSearchButton;
     private DrawerLayout mainDrawerLayout;
+    private NavigationView mainNavigationView;
     private Toolbar toolbar;
+    private Context mContext=MainActivity.this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +51,7 @@ public class MainActivity extends AppCompatActivity {
         ivToolbarSearchButton=findViewById(R.id.iv_toolbarSearchButton);
         mainDrawerLayout=findViewById(R.id.layout_mainDrawer);
         toolbar=findViewById(R.id.toolbar_main);
+        mainNavigationView=findViewById(R.id.navigation);
     }
 
     private void initListener(){
@@ -60,6 +68,19 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent=new Intent(MainActivity.this, MapActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        mainNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id=item.getItemId();
+                Log.d(TAG, "onNavigationItemSelected: id : "+id);
+                if (id==R.id.menu_housemenual){
+                    Intent intent=new Intent(mContext, MoveInActivity.class);
+                    startActivity(intent);
+                }
+                return true;
             }
         });
     }
---
+++ app/src/main/java/com/mary/sharehouseproject/fragment/fragment/HouseManualViewPager3.java
@@ -0,0 +1,22 @@
+package com.mary.sharehouseproject.fragment.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.mary.sharehouseproject.R;
+
+
+public class HouseManualViewPager3 extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v=inflater.inflate(R.layout.housemanual_viewpage3,container,false);
+        return v;
+    }
+}
