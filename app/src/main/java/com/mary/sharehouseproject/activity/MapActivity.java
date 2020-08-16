80696c4064c7482dd74f5dcbf8859166120cecc8
Mary <qldhffk123@gmail.com>
2020-08-16 15:57:48 +0900

    TedNaverClustering API 구현 완료

--- app/src/main/java/com/mary/sharehouseproject/activity/MapActivity.java
+++ app/src/main/java/com/mary/sharehouseproject/activity/MapActivity.java
@@ -1,24 +1,56 @@
 package com.mary.sharehouseproject.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.content.Context;
 import android.os.Bundle;
 
 import com.mary.sharehouseproject.R;
+import com.mary.sharehouseproject.model.HouseTest;
+import com.naver.maps.geometry.LatLngBounds;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.CameraUpdate;
+import com.naver.maps.map.MapFragment;
+import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.NaverMapSdk;
 
+import java.util.ArrayList;
+import java.util.List;
 
-public class MapActivity extends AppCompatActivity {
+import lombok.NonNull;
+import ted.gun0912.clustering.naver.TedNaverClustering;
 
-    private Context mapContext=MapActivity.this;
+
+public class MapActivity extends BaseDemoActivity {
+    private NaverMap naverMap;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-        NaverMapSdk.getInstance(this).setClient(
-                new NaverMapSdk.NaverCloudPlatformClient("x"));
+    public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
+
+        naverMap.moveCamera(
+                CameraUpdate.toCameraPosition(
+                        new CameraPosition(NaverMap.DEFAULT_CAMERA_POSITION.target, NaverMap.DEFAULT_CAMERA_POSITION.zoom))
+        );
+
+        TedNaverClustering.with(this, naverMap)
+                .items(getItems())
+                .make();
+
+    }
+
+
+    private List<HouseTest> getItems() {
+        LatLngBounds bounds = naverMap.getContentBounds();
+        ArrayList<HouseTest> items = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            HouseTest temp = new HouseTest((bounds.getNorthLatitude() - bounds.getSouthLatitude()) * Math.random() + bounds.getSouthLatitude(),
+                    (bounds.getEastLongitude() - bounds.getWestLongitude()) * Math.random() + bounds.getWestLongitude()
+            );
+            items.add(temp);
+        }
+        return items;
 
     }
 }
