451c75fee423b8872ddb5079baaab337917ec39d
Mary <qldhffk123@gmail.com>
2020-08-18 01:28:35 +0900

    뷰에 이미지 추가

--- app/src/main/java/com/mary/sharehouseproject/fragment/LendhouseViewPager2.java
+++ app/src/main/java/com/mary/sharehouseproject/fragment/LendhouseViewPager2.java
@@ -4,19 +4,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.bumptech.glide.Glide;
 import com.mary.sharehouseproject.R;
 
 
 public class LendhouseViewPager2 extends Fragment {
+    private static final String TAG = "LendhouseViewPager2";
+
+    private ImageView ivAdventage1;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.lendhouse_viewpage2,container,false);
+
+
         return v;
     }
+
 }
