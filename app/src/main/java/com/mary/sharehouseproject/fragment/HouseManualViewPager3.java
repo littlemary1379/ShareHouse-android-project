d6501bd6964d05321ce481394be398e92af93ae1
mary <qldhffk112@gmail.com>
2020-08-19 15:21:33 +0900

    제휴 서비스의 버튼 클릭 시 각 액티비티로 이동 구현

--- app/src/main/java/com/mary/sharehouseproject/fragment/HouseManualViewPager3.java
+++ app/src/main/java/com/mary/sharehouseproject/fragment/HouseManualViewPager3.java
@@ -1,22 +1,56 @@
 package com.mary.sharehouseproject.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.mary.sharehouseproject.R;
+import com.mary.sharehouseproject.activity.LoginActivity;
+import com.mary.sharehouseproject.activity.SearchActivity;
 
 
 public class HouseManualViewPager3 extends Fragment {
+    private Button btnSearchRoom, btnGetJoin;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View v=inflater.inflate(R.layout.housemanual_viewpage3,container,false);
+        init(v);
+        listener();
         return v;
     }
+
+    private void init(View v){
+        btnGetJoin=v.findViewById(R.id.btn_getJoin);
+        btnSearchRoom=v.findViewById(R.id.btn_searchRoom);
+    }
+
+    private void listener(){
+        btnSearchRoom.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getContext(), SearchActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+            }
+        });
+
+        btnGetJoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getContext(), LoginActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+            }
+        });
+    }
+
 }
